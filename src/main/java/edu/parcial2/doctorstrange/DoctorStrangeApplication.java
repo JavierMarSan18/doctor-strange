392aa8f6c5e3392830a177de791b7dc4dbf18593
JavierMarSan18 <marroquinjavier75@gmail.com>
2022-05-06 00:49:45 -0600

    Initial commit

---
+++ src/main/java/edu/parcial2/doctorstrange/DoctorStrangeApplication.java
@@ -0,0 +1,13 @@
+package edu.parcial2.doctorstrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorStrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorStrangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/parcial2/doctorstrange/DoctorStrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.parcial2.doctorstrange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorStrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
